ff1a2de6194c263779970a3cbd289f2ffae7157a
Bishakha143 <148806470+Bishakha143@users.noreply.github.com>
2023-11-02 17:09:43 +0530

    Add files via upload

---
+++ first1.py
@@ -0,0 +1,11 @@
+#name= input("What is your name?")
+#print("Hello" + name)
+#print("how are you?")
+
+number= float(input("entere the number"))
+if number> 0:
+    print("The number is positive")
+elif number== 0:
+    print("The numer is Zero")
+else:
+ print("The number is negative")
